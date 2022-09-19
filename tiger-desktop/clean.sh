ef832ed5f9c7cce71ade1de33bb5e73337adbec8
charles.lee <cheoljoo.lee@lge.com>
2022-09-19 12:16:58 +0900

    add tiger-desktop download script

---
+++ tiger-desktop/clean.sh
@@ -0,0 +1,17 @@
+rm -rf build_apps.sh
+mv apps_proc a 
+rm -rf a &
+mv a35 aaa
+rm -rf aaa &
+mv intel-build i 
+rm -rf i &
+mv .repo r
+rm -rf r &
+mv nad n 
+rm -rf n &
+rm -f run-docker.sh
+mv dummy d
+rm -rf d &
+rm -f m
+
+#a35  apps_proc  build_apps.sh  clean.sh  down.sh  dummy  intel-build
---
+++ tiger-desktop/down.sh
@@ -0,0 +1,21 @@
+#/data01/cheoljoo.lee/bin/repo init -u ssh://vgit.lge.com:29420/tiger/manifest/ -b tiger_release -m tiger_desktop_gen12_release.xml
+repo init -u ssh://vgit.lge.com:29420/tiger/manifest/ -b tiger_release -m tiger_desktop_release.xml
+repo sync -cq -j8
+repo start tiger_release --all
+echo "cd apps_proc/lge/servicelayer/core/services/"
+echo "cd tiger-desktop-gen12/intel-build/poky/meta-tiger/classes"
+echo "tiger-feature-common.bbclass"
+echo "#do_fetch[nostamp] = 1" 
+
+echo "$ mkdir  my_tiger_src ;     cd  my_tiger_src"
+echo "$ repo init -u ssh://vgit.lge.com:29420/tiger/manifest/ -b tiger_release -m  tiger_desktop_gen12_release.xml"
+echo "$ repo sync -cqj 8  ;    repo start tiger_release --all"
+
+echo "$  ./run-docker.sh                  # help command"
+echo "$  ./run-docker.sh build apps       # build"
+echo "$  ./run-docker.sh run              # Run tiger-desktop"
+echo "$  ./run-docker.sh committest       # Run commit test"
+echo "$  ./run-docker.sh coverity         # Run coverity source code"
+echo "$  ./run-docker.sh uts              # Run unit test"
+
+# vi ./intel-build/poky/meta-tiger/classes/tiger-feature-common.bbclass
