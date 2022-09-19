3597b8a85e2139184e33940970bcd0468a08eff9
charles.lee <cheoljoo.lee@lge.com>
2022-09-19 16:52:49 +0900

    add necessary tool

--- tiger-desktop/down.sh
+++ tiger-desktop/down.sh
@@ -1,7 +1,7 @@
 #/data01/cheoljoo.lee/bin/repo init -u ssh://vgit.lge.com:29420/tiger/manifest/ -b tiger_release -m tiger_desktop_gen12_release.xml
-repo init -u ssh://vgit.lge.com:29420/tiger/manifest/ -b tiger_release -m tiger_desktop_release.xml
-repo sync -cq -j8
-repo start tiger_release --all
+../repo init -u ssh://vgit.lge.com:29420/tiger/manifest/ -b tiger_release -m tiger_desktop_release.xml
+../repo sync -cq -j8
+../repo start tiger_release --all
 echo "cd apps_proc/lge/servicelayer/core/services/"
 echo "cd tiger-desktop-gen12/intel-build/poky/meta-tiger/classes"
 echo "tiger-feature-common.bbclass"
