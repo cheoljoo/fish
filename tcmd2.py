95617f1ff30845bcb8250f656a84b1cf4ebc20fd
charles.lee <cheoljoo.lee@lge.com>
2022-10-02 16:45:51 +0900

    add example : tcmd2.py - using dictionary instead of list. dictionary will have the description as value

---
+++ tcmd2.py
@@ -0,0 +1,95 @@
+import CiscoStyleCli
+import sys
+
+#print(help(CiscoStyleCli))
+
+csc = CiscoStyleCli.CiscoStyleCli(prompt="TCMD:>")
+#print(csc.__doc__)
+
+TOP = {}
+projectList = ['tiger_desktop_release' , 'bmw_icon_nad_release' , 'toyota_24dcm_release' , 'tiger_desktop_honda_release' , 'tiger_desktop_gen12_release']
+projectDict = {
+    'tiger_desktop_release' : 'tiger release on x86',
+    'bmw_icon_nad_release' : 'BMW iconnic sf24' , 
+    'toyota_24dcm_release' : 'TOYOTA 24cy dcm', 
+    'tiger_desktop_honda_release' : 'HONDA 23MY' , 
+    'tiger_desktop_gen12_release' : 'GM gen12'
+}
+TOP ['src'] = {
+    '__attribute' : {
+        'desc' : "get source code",
+    },
+    'project' : {
+        '__attribute' : {
+            'type' : "argument",
+            'desc' : "project name",
+            'argument-type': projectList,
+            'returnable' : "returnable",
+        },
+        'target' : {
+            '__attribute' : {
+                'type' : "argument",
+                'desc' : "target",
+                'argument-type': 'str',
+                'returnable' : "returnable",
+            },
+        }
+    },
+}
+TOP ['run'] = {
+    '__attribute' : {
+        'desc' : "run",
+    },
+    'desktop' : {
+        '__attribute' : {
+            'desc' : "desktop",
+        },
+        'committest' : {
+            '__attribute' : {
+                'desc' : "committest",
+                'type' : 'command',
+                'returnable' : "returnable",
+            }
+        }
+    }
+}
+TOP ['test'] = {
+    '__attribute' : {
+        'desc' : "build & test",
+    },
+    'daily' : {
+        '__attribute' : {
+            'desc' : "daily",
+        },
+        'project' : {
+            '__attribute' : {
+                'desc' : "project name",
+                'type' : 'argument',
+                'argument-type': projectList,
+                'returnable' : "returnable",
+            }
+        }
+    },
+    'vt' : {
+        '__attribute' : {
+            'desc' : "vbee test",
+        },
+        'project' : {
+            '__attribute' : {
+                'desc' : "project name",
+                'type' : 'argument',
+                'argument-type': projectDict,
+                'returnable' : "returnable",
+            }
+        }
+    }
+}
+csc.setCliRuleTcmd(TOP)
+
+if len(sys.argv) > 1:
+    print("argv:",sys.argv)
+    x = ' '.join(sys.argv[1:])
+    csc.runCommand(x)
+else :
+    csc.run()
+
